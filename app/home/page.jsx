e1b1f48eb4551f9b4e1147e193004dc3c7c2c43f
Ahmet İncekara <ahmetincekaraa@gmail.com>
2024-09-21 00:56:00 +0300

    Ordering

---
+++ app/home/page.jsx
@@ -0,0 +1,7 @@
+const page = () => {
+  return (
+    <div className="text-red-500 font-dancing text-3xl">Home Page</div>
+  )
+}
+
+export default page
---
+++ app/page.jsx
@@ -0,0 +1,15 @@
+import Home from "./home/page"
+
+export default function Index() {
+  return (
+  
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+     <link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&display=swap" rel="stylesheet" />
+<Home/>
+
+    </div>
+  
+  );
+}
