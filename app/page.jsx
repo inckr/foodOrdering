d7c18834ba4419a777439a7d5f20570b608cc68e
Ahmet İncekara <ahmetincekaraa@gmail.com>
2024-09-26 00:40:21 +0300

    Header, Title güncellemeleri

--- app/page.jsx
+++ app/page.jsx
@@ -2,15 +2,17 @@ import Home from "./home/page"
 import Header from "@/components/layout/Header";
 export default function Index() {
   return (
-  
+  <body>
+    
     <div className="">
      <link rel="preconnect" href="https://fonts.googleapis.com" />
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-
+<div>
 <Header/>
 <Home/>
-
+</div>
     </div>
   
+    </body>
   );
 }
--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,40 +1,46 @@
+"use client";
+
+import { useState } from "react";
 import Logo from "../ui/Logo"
+import Title from "../ui/Title"
 import { FaUserAlt, FaSearch  } from "react-icons/fa";
 import { FaCartShopping } from "react-icons/fa6";
+import OutsideClickHandler from 'react-outside-click-handler';
 
 const Header = () => {
+  const [isSearchModal, setIsSearchModal] = useState(false);
   return (
-    <div className="h-[5.5rem] bg-secondary h-full">
-       <div className="container mx-auto text-white flex justify-between ">
+    <div className="">
+       <div className="container mx-auto text-white flex justify-between h-[5.5rem] bg-secondary h-full">
       
             <Logo/>
         
         <nav className="h-full">
           <ul className="flex gap-x-2">
-            <li className="px-[0.313rem] py-[0.9rem] uppercase hover:text-primary cursor-pointer">
+            <li className="px-[0.313rem] py-[0.9rem] uppercase hover:text-primary cursor-pointer transition-all">
               <a href="">Home</a>
             </li>
-            <li className="px-[0.313rem] py-[0.9rem] uppercase hover:text-primary cursor-pointer">
+            <li className="px-[0.313rem] py-[0.9rem] uppercase hover:text-primary cursor-pointer transition-all">
               <a href="">Menu</a>
             </li>
-            <li className="px-[0.313rem] py-[0.9rem] uppercase hover:text-primary cursor-pointer">
+            <li className="px-[0.313rem] py-[0.9rem] uppercase hover:text-primary cursor-pointer transition-all">
               <a href="">About</a>
             </li>
-            <li className="px-[0.313rem] py-[0.9rem] uppercase hover:text-primary cursor-pointer">
+            <li className="px-[0.313rem] py-[0.9rem] uppercase hover:text-primary cursor-pointer transition-all">
               <a href="">Book Table</a>
             </li>
           </ul>
         </nav>
         <div className="flex gap-x-4 items-center">
           <a href="">
-          <FaUserAlt />
+          <FaUserAlt className="hover:text-primary transition-all"/>
           </a>
           <a href="">
-          <FaCartShopping />
+          <FaCartShopping className="hover:text-primary transition-all"/>
           </a>
-          <a href="">
-          <FaSearch />
-          </a>
+          <button onClick={() => setIsSearchModal(true)}>
+          <FaSearch className="hover:text-primary transition-all"/>
+          </button>
           <a href="">
             <button className="btn-primary">
               Order Online
@@ -42,6 +48,13 @@ const Header = () => {
           </a>
         </div>
        </div>
+       {isSearchModal &&(
+        <OutsideClickHandler onOutsideClick={() => setIsSearchModal(false)}>
+<div className="">
+  <Title addClass="text-9xl">Title</Title>
+</div>
+        </OutsideClickHandler>
+       ) }
     </div>
   )
 }
