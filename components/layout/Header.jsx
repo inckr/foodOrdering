395fc5468ddbcef766b216087d338e040629de98
Ahmet İncekara <ahmetincekaraa@gmail.com>
2024-09-26 01:58:37 +0300

    Search sayfası oluşturma

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,21 +1,20 @@
 "use client";
-
+import Search from "../ui/Search";
 import { useState } from "react";
 import Logo from "../ui/Logo"
-import Title from "../ui/Title"
 import { FaUserAlt, FaSearch  } from "react-icons/fa";
 import { FaCartShopping } from "react-icons/fa6";
-import OutsideClickHandler from 'react-outside-click-handler';
+
 
 const Header = () => {
   const [isSearchModal, setIsSearchModal] = useState(false);
   return (
-    <div className="">
-       <div className="container mx-auto text-white flex justify-between h-[5.5rem] bg-secondary h-full">
+    <div className="h-[5.5rem] bg-secondary">
+       <div className="container mx-auto text-white flex justify-between items-center h-full">
       
             <Logo/>
         
-        <nav className="h-full">
+        <nav>
           <ul className="flex gap-x-2">
             <li className="px-[0.313rem] py-[0.9rem] uppercase hover:text-primary cursor-pointer transition-all">
               <a href="">Home</a>
@@ -48,14 +47,12 @@ const Header = () => {
           </a>
         </div>
        </div>
+       <div>
        {isSearchModal &&(
-        <OutsideClickHandler onOutsideClick={() => setIsSearchModal(false)}>
-<div className="">
-  <Title addClass="text-9xl">Title</Title>
-</div>
-        </OutsideClickHandler>
+        <Search setIsSearchModal={setIsSearchModal}/>
        ) }
-    </div>
+       </div>
+   </div>
   )
 }
 
---
+++ components/ui/Search.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import Title from "../ui/Title"
+import Image from 'next/image';
+import OutsideClickHandler from 'react-outside-click-handler';
+import { FaRegWindowClose  } from "react-icons/fa";
+
+const Search = ({setIsSearchModal}) => {
+  return (
+    <div className="fixed position w-screen h-screen z-50 top-0 left-0 after:content-[''] after:w-screen after:h-screen after:bg-white after:absolute after:top-0 after:left-0 after:opacity-40 grid place-content-center">
+        <OutsideClickHandler onOutsideClick={() => setIsSearchModal(false)}>
+<div className='w-full h-full grid place-content-center'>
+<div className="relative z-50 w-[600px] h-[600px] bg-gray-300 border-2 rounded-2xl p-5">
+  <Title addClass="text-[40px] text-center">Search</Title>
+  <input type="text" placeholder='Search..' className='border w-full my-10' />
+  <div>
+    <ul >
+        <li className='flex items-center justify-between p-1 hover:bg-primary transition-all'>
+            <div className='relative flex'>
+                <Image src="/images/f1.png" alt=""
+                width={50}
+                height={50}
+                />
+            </div>
+            <span className='font-bold'>Pizza</span>
+            <span className='font-bold'>300₺</span>
+        </li>
+        <li className='flex items-center justify-between p-1 hover:bg-primary transition-all'>
+            <div className='relative flex'>
+                <Image src="/images/f1.png" alt=""
+                width={50}
+                height={50}
+                />
+            </div>
+            <span className='font-bold'>Pizza</span>
+            <span className='font-bold'>300₺</span>
+        </li>
+        <li className='flex items-center justify-between p-1 hover:bg-primary transition-all'>
+            <div className='relative flex'>
+                <Image src="/images/f1.png" alt=""
+                width={50}
+                height={50}
+                />
+            </div>
+            <span className='font-bold'>Pizza</span>
+            <span className='font-bold'>300₺</span>
+        </li>
+    </ul>
+    <button onClick={() => setIsSearchModal(false)} className='absolute top-4 right-4'>
+    <FaRegWindowClose  size={20} className=' hover:text-red-700 transition-all'/>
+    </button>
+  </div>
+</div>
+</div>
+        </OutsideClickHandler>
+    </div>
+  )
+}
+
+export default Search
