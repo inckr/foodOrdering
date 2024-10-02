d70512af9e3a8f30799714159bd7ebf8f4744628
Ahmet İncekara <ahmetincekaraa@gmail.com>
2024-10-02 20:15:41 +0300

    Responsive düzenlemeleri

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -4,18 +4,22 @@ import { useState } from "react";
 import Logo from "../ui/Logo"
 import { FaUserAlt, FaSearch  } from "react-icons/fa";
 import { FaCartShopping } from "react-icons/fa6";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { FaRegWindowClose  } from "react-icons/fa";
+
 
 
 const Header = () => {
   const [isSearchModal, setIsSearchModal] = useState(false);
+  const [isMenuModal, setIsMenuModal] = useState(false);
   return (
     <div className="h-[5.5rem] bg-secondary">
        <div className="container mx-auto text-white flex justify-between items-center h-full">
       
             <Logo/>
         
-        <nav>
-          <ul className="flex gap-x-2">
+        <nav className={`sm:static absolute top-0 left-0 grid place-content-center sm:w-auto sm:h-auto w-full h-full sm:text-white text-black sm:bg-transparent bg-gray-300 ${isMenuModal ? "block" : "hidden"} sm:block`}>
+          <ul className="flex gap-x-2 sm:flex-row flex-col items-center">
             <li className="px-[0.313rem] py-[0.9rem] uppercase hover:text-primary cursor-pointer transition-all">
               <a href="">Home</a>
             </li>
@@ -29,6 +33,13 @@ const Header = () => {
               <a href="">Book Table</a>
             </li>
           </ul>
+          {isMenuModal && (
+            <button 
+            className='absolute top-4 right-4 z-50'
+            onClick={() => setIsMenuModal(false)} >
+            <FaRegWindowClose  size={20} className=' hover:text-red-700 transition-all'/>
+            </button>
+          )}
         </nav>
         <div className="flex gap-x-4 items-center">
           <a href="">
@@ -40,11 +51,14 @@ const Header = () => {
           <button onClick={() => setIsSearchModal(true)}>
           <FaSearch className="hover:text-primary transition-all"/>
           </button>
-          <a href="">
+          <a href="" className="md:inline-block hidden">
             <button className="btn-primary">
               Order Online
             </button>
           </a>
+          <button onClick={() => setIsMenuModal(true)} className="sm:hidden inline-block">
+          <RxHamburgerMenu className="text-xl hover:text-primary transition-all"/>
+          </button>
         </div>
        </div>
        <div>
